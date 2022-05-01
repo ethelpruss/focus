cd1fa0b7e2f4a7c9dc533503d01cf2b24e536ae0
ethelpruss <47779830+ethelpruss@users.noreply.github.com>
2022-05-01 05:05:34 +0200

    Update index.py

--- index.py
+++ index.py
@@ -7,8 +7,6 @@ from app import server
 from layouts import explore, analyze, present
 import callbacks
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.DARKLY])
-server = app.server
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
